5b1c5130baa16960e55063236ae3be6258491d3b
0xiaoxi <xiaoxi.lin@jiyinbio.com>
2025-10-14 16:57:38 +0800

    Update BrowserWindow options

--- src/main/index.ts
+++ src/main/index.ts
@@ -13,11 +13,8 @@ function createWindow(): void {
     ...(process.platform === 'linux' ? { icon } : {}),
     center: true,
     title: 'Markdown-Note',
-    frame: false,
     vibrancy: 'under-window',
     visualEffectState: 'active',
-    titleBarStyle: 'hidden',
-    trafficLightPosition: { x: 15, y: 10 },
     webPreferences: {
       preload: join(__dirname, '../preload/index.js'),
       sandbox: true,
